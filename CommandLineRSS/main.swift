fce08d437f32edfe50a134f191c346f8bdd5536a
Fabian Schneider <fabi@FabiMac.local>
2015-07-01 21:31:03 +0200

    Added command line arguments to set the URL dynamically

--- CommandLineRSS/main.swift
+++ CommandLineRSS/main.swift
@@ -1,69 +1,71 @@
 import Foundation
 
-//########################################################
-// ToDo-List:
-//      - Private / Public / etc.
-//      - Extend RSSXMLDelegate with Map/List for RSS-Item
-//        objects
-//      - Check warnings
-//      - Check if "if let x..." also works with the 
-//        !-operator
-//########################################################
-
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // CommandLineRSS
 // Simple RSS Reader made with Swift
 // fabian (at) fabianschneider.org
 // GPL V2
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-var rssURL : NSURL? = NSURL( string: "http://blog.fefe.de/rss.xml" )
-var rssItems = [RSSItem]()
+//Parsing command line arguments
 
-if let url = rssURL {
-    var xmlParser : NSXMLParser? = NSXMLParser( contentsOfURL: rssURL )
-    var xmlDelegate : RSSXMLDelegate = RSSXMLDelegate()
+if( Process.argc == 2 ) {
+	
+	//Get String from argument
+	var urlString : String = Process.arguments[ 1 ]
+
+	var rssURL : NSURL? = NSURL( string: urlString )
+	var rssItems = [RSSItem]()
+
+	if let url = rssURL {
+    	var xmlParser : NSXMLParser? = NSXMLParser( contentsOfURL: rssURL )
+    	var xmlDelegate : RSSXMLDelegate = RSSXMLDelegate()
 
    
-    if let parser = xmlParser {
-        parser.delegate = xmlDelegate
+    	if let parser = xmlParser {
+        	parser.delegate = xmlDelegate
 
-        if( !parser.parse() ) {
-            //Error while parsing the XML file
-            println( "Error while parsing the XML file" )
-        } else {
-            rssItems = xmlDelegate.getItemsList()
+        	if( !parser.parse() ) {
+            	//Error while parsing the XML file
+            	println( "Error while parsing the XML file" )
+        	} else {
+            	rssItems = xmlDelegate.getItemsList()
             
-            for item in rssItems {
+           		for item in rssItems {
+
+                	if let title = item.getTitle() {
+                    	println( title )
+                	}
 				
-                if let title = item.getTitle() {
-                    println( title )
-                }
+					if let description = item.getDescription() {
+						println( description )
+					}
 				
-				if let description = item.getDescription() {
-					println( description )
-				}
+                	if let link = item.getLink() {
+                    	println( link )
+                	}
 				
-                if let link = item.getLink() {
-                    println( link )
-                }
+					if let author = item.getAuthor() {
+						println( author )
+					}
 				
-				if let author = item.getAuthor() {
-					println( author )
-				}
+					if let guid = item.getGUID() {
+						println( guid )
+					}
 				
-				if let guid = item.getGUID() {
-					println( guid )
-				}
+                	if let date = item.getDate() {
+                    	println( date )
+                	}
 				
-                if let date = item.getDate() {
-                    println( date )
-                }
-				
-				println()
-            }
-        }
-    }
+					println()
+            	}
+        	}
+    	}
+	}
+} else {
+	println( "Missing Argument!" )
+	println( "CommandLineRSS $URL_TO_RSSFEED" )
+	println( "Example:" )
+	println( "\t CommandLineRSS http://www.faz.net/rss/aktuell/" )
 }
 
